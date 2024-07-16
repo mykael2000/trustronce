9fa390ce94a69df3a3eec9622a79bfd49f69a445
Erameh Michael <eramehmichael2000@gmail.com>
2024-07-16 17:39:58 +0100

    added

--- dash/dashboard/main.php
+++ dash/dashboard/main.php
@@ -114,32 +114,6 @@ if(!empty($row['withdrawal_status'])){
     <div class="row">
         <div class="col-lg-12">
             <div class="row align-items-end mb-1">
-                        <div class="col-6 block p-1 px-1">
-                            <div class="card shining-card">
-                                <div class="card-body">
-
-                                    <span style="font-size: 13px;" class="me-2">Total Balance</span>
-                                    <svg width="10" height="10" viewBox="0 0 36 35" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                            d="M3.86124 21.6224L11.2734 16.8577C11.6095 16.6417 12.041 16.6447 12.3718 16.8655L18.9661 21.2663C19.2968 21.4871 19.7283 21.4901 20.0644 21.2741L27.875 16.2534"
-                                            stroke="#BFBFBF" stroke-linecap="round" stroke-linejoin="round" />
-                                        <path
-                                            d="M26.7847 13.3246L31.6677 14.0197L30.4485 18.7565L26.7847 13.3246ZM30.2822 19.4024C30.2823 19.4023 30.2823 19.4021 30.2824 19.402L30.2822 19.4024ZM31.9991 14.0669L31.9995 14.0669L32.0418 13.7699L31.9995 14.0669C31.9994 14.0669 31.9993 14.0669 31.9991 14.0669Z"
-                                            fill="#BFBFBF" stroke="#BFBFBF" />
-                                    </svg>
-                                    <div class="pt-3">
-                                        <h4 class="counter" style="visibility: visible; font-size: 16px;">
-                                            $<?php echo number_format($row['total_balance'], 2, '.', ',').' '; ?>USD
-                                        </h4>
-
-                                    </div>
-                                    <div class="d-flex justify-content-center">
-                                        <a class="action-button" style="font-size: 12px;" href="withdrawal.php">Withdraw</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                         <div class=" col-6 d-block p-1 px-1">
                             <div class="card shining-card">
                                 <div class="card-body">
@@ -162,6 +136,32 @@ if(!empty($row['withdrawal_status'])){
                                     </div>
                                     <div class="d-flex justify-content-center">
                                         <a class="action-button" style="font-size: 12px;" href="deposit.php" >Deposit</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-6 block p-1 px-1">
+                            <div class="card shining-card">
+                                <div class="card-body">
+
+                                    <span style="font-size: 13px;" class="me-2">Total Balance</span>
+                                    <svg width="10" height="10" viewBox="0 0 36 35" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M3.86124 21.6224L11.2734 16.8577C11.6095 16.6417 12.041 16.6447 12.3718 16.8655L18.9661 21.2663C19.2968 21.4871 19.7283 21.4901 20.0644 21.2741L27.875 16.2534"
+                                            stroke="#BFBFBF" stroke-linecap="round" stroke-linejoin="round" />
+                                        <path
+                                            d="M26.7847 13.3246L31.6677 14.0197L30.4485 18.7565L26.7847 13.3246ZM30.2822 19.4024C30.2823 19.4023 30.2823 19.4021 30.2824 19.402L30.2822 19.4024ZM31.9991 14.0669L31.9995 14.0669L32.0418 13.7699L31.9995 14.0669C31.9994 14.0669 31.9993 14.0669 31.9991 14.0669Z"
+                                            fill="#BFBFBF" stroke="#BFBFBF" />
+                                    </svg>
+                                    <div class="pt-3">
+                                        <h4 class="counter" style="visibility: visible; font-size: 16px;">
+                                            $<?php echo number_format($row['total_balance'], 2, '.', ',').' '; ?>USD
+                                        </h4>
+
+                                    </div>
+                                    <div class="d-flex justify-content-center">
+                                        <a class="action-button" style="font-size: 12px;" href="withdrawal.php">Withdraw</a>
                                     </div>
                                 </div>
                             </div>
