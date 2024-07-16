2651ad47c9b29351f5cd56b67ef7c406f89be56e
Erameh Michael <eramehmichael2000@gmail.com>
2024-07-16 17:29:37 +0100

    added

--- dash/dashboard/admin/edit.php
+++ dash/dashboard/admin/edit.php
@@ -6,9 +6,9 @@ use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
 
-require '../PHPMailer-master/src/PHPMailer.php';
-require '../PHPMailer-master/src/Exception.php';
-require '../PHPMailer-master/src/SMTP.php';
+require '../../PHPMailer-master/src/PHPMailer.php';
+require '../../PHPMailer-master/src/Exception.php';
+require '../../PHPMailer-master/src/SMTP.php';
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
  
