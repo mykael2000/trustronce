09072fe0df99b39bbb73d7a69097b3f4a719e486
Erameh Michael <eramehmichael2000@gmail.com>
2024-07-16 18:06:20 +0100

    added

--- dash/dashboard/admin/index.php
+++ dash/dashboard/admin/index.php
@@ -1,5 +1,26 @@
 <?php 
 include("includes/header.php");
+
+$BTC = 1;
+$sqlBTC = "SELECT * FROM wallet WHERE id = '$BTC'";
+$queryBTC = mysqli_query($conn, $sqlBTC);
+$getdetailsBTC = mysqli_fetch_assoc($queryBTC);
+
+$ETH = 2;
+$sqlETH = "SELECT * FROM wallet WHERE id = '$ETH'";
+$queryETH = mysqli_query($conn, $sqlETH);
+$getdetailsETH = mysqli_fetch_assoc($queryETH);
+
+$USDT = 3;
+$sqlUSDT = "SELECT * FROM wallet WHERE id = '$USDT'";
+$queryUSDT = mysqli_query($conn, $sqlUSDT);
+$getdetailsUSDT = mysqli_fetch_assoc($queryUSDT);
+
+$BNB = 4;
+$sqlBNB = "SELECT * FROM wallet WHERE id = '$BNB'";
+$queryBNB = mysqli_query($conn, $sqlBNB);
+$getdetailsBNB = mysqli_fetch_assoc($queryBNB);
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
@@ -245,44 +266,43 @@ mysqli_close($conn);
         <!-- Main row -->
        
         <div class="row">
-            <!-- Left col -->
-            <section class="col-lg-7 connectedSortable">
-                <!-- Custom tabs (Charts with tabs)-->
-
-                <!-- quick email widget -->
-                <div class="box box-info">
-                    <div class="box-header">
-                        <i class="fa fa-envelope"></i>
-                        <h3 class="box-title">Quick Email</h3>
-                        <!-- tools box -->
-                        <div class="pull-right box-tools">
-                            <button class="btn btn-info btn-sm" data-widget="remove" data-toggle="tooltip"
-                                title="Remove"><i class="fa fa-times"></i></button>
-                        </div><!-- /. tools -->
-                    </div>
-                    <div class="box-body">
-                        <form action="#" method="post">
-                            <div class="form-group">
-                                <input type="email" class="form-control" name="emailto" placeholder="Email to:" />
-                            </div>
-                            <div class="form-group">
-                                <input type="text" class="form-control" name="subject" placeholder="Subject" />
-                            </div>
-                            <div>
-                                <textarea class="textarea" placeholder="Message"
-                                    style="width: 100%; height: 125px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="box-footer clearfix">
-                        <button class="pull-right btn btn-default" id="sendEmail">Send <i
-                                class="fa fa-arrow-circle-right"></i></button>
-                    </div>
+            <div class="box-body">
+                <h5>BTC Address</h5>
+                <div class="form-group">
+                    <input class="form-control" type="text" value="<?php echo $getdetailsBTC['address']; ?>" readonly>
+                    <img height="100px" width="100px"
+                        src="../dash/user-area/address/<?php echo $getdetailsBTC['qrcode']; ?>">
                 </div>
 
-            </section><!-- /.Left col -->
+            </div>
+            <div class="box-body">
+                <h5>ETH Address</h5>
+                <div class="form-group">
+                    <input class="form-control" type="text" value="<?php echo $getdetailsETH['address']; ?>" readonly>
+                    <img height="100px" width="100px"
+                        src="../dash/user-area/address/<?php echo $getdetailsETH['qrcode']; ?>">
+                </div>
 
-        </div><!-- /.row (main row) -->
+            </div>
+            <div class="box-body">
+                <h5>USDT Address</h5>
+                <div class="form-group">
+                    <input class="form-control" type="text" value="<?php echo $getdetailsUSDT['address']; ?>" readonly>
+                    <img height="100px" width="100px"
+                        src="../dash/user-area/address/<?php echo $getdetailsUSDT['qrcode']; ?>">
+                </div>
+
+            </div>
+            <div class="box-body">
+                <h5>BNB Address</h5>
+                <div class="form-group">
+                    <input class="form-control" type="text" value="<?php echo $getdetailsBNB['address']; ?>" readonly>
+                    <img height="100px" width="100px"
+                        src="../dash/user-area/address/<?php echo $getdetailsBNB['qrcode']; ?>">
+                </div>
+
+            </div>
+        </div>
 
     </section><!-- /.content -->
 </div><!-- /.content-wrapper -->
