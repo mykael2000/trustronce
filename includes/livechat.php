799991162aec86f3ce12a6b3d3988afc96a61048
Erameh Michael <eramehmichael2000@gmail.com>
2024-08-06 17:22:08 +0100

    livechat

--- includes/livechat.php
+++ includes/livechat.php
@@ -1,7 +1,7 @@
 <!-- Smartsupp Live Chat script -->
 <script type="text/javascript">
 var _smartsupp = _smartsupp || {};
-_smartsupp.key = '064cebedc1b3fe5c081ef0b3dbe9a91ea2f54af2';
+_smartsupp.key = 'cf7e4ae0c900692a1ae5e2757199a7bdd91e9c8d';
 window.smartsupp||(function(d) {
   var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
   s=d.getElementsByTagName('script')[0];c=d.createElement('script');
